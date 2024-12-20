ab1dc0d8c7b47824ac05f0425ffc8df5b1202b67
gbogdanova <gaaalo44ka@gmail.com>
2024-12-20 15:06:37 -0800

    feat: add start/stop timer

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,10 +1,33 @@
+import { useState, useRef } from "react"
+
 export default function TimerChallenge ({title, targetTime}) {
+  const timer = useRef();
+  const [timerStart, setTimerStart] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart(){
+    setTimerStart(true);
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTime * 1000);
+  }
+
+  function handleStop(){
+    clearTimeout(timer.current);
+    setTimerStart(false);
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">{targetTime} second{targetTime > 1 ? 's' : ''}</p>
-      <p><button>Start Chalenge</button></p>
-      <p className="">Time is running.../Timer inactive</p>
+      <p>
+        <button onClick={timerStart? handleStop : handleStart}>
+          {timerStart ? 'Stop ' : 'Start '}Chalenge
+        </button>
+      </p>
+      <p className={timerStart ? 'active' : undefined}>{timerStart ? 'Time is running...' : 'Timer inactive'}</p>
     </section>
       
   )
